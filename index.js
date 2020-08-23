5f7824de24c9f834e4317ac6394c3693ed161085
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-23 21:20:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function BoardMember(name,homeState,training)
 {
    this.name=name;
    this.homeState=homeState;
-   this.training;
+   this.training=training;
 }
  BoardMember.prototype.veto=function()
  {
