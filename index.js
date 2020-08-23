677dfdfbd5f39c8e4816e9c48e983617133087d6
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-08-23 21:22:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function BoardMember(name,homeState,training)
  {
    return 'I like to help people';
  }
+
+ BoardMember.prototype.releasePressStatement=function()
+ {
+   return 'You will see great things from Scuber';
+ }
